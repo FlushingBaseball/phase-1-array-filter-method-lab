88fd3a83191812b386dab13abc18dc40ef1637d5
Sam Peck <fullstacksam@protonmail.com>
2023-05-19 14:41:21 -0400

    finished the lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+
+// return driverNameArray.filter(string)
+
+
+
+function findMatching(driverNameArray, string){
+   let matches = driverNameArray.filter(driver => driver.toLowerCase() ===string.toLowerCase())
+   return matches
+}
+
+function fuzzyMatch(driverNameArray, string){
+    let fuzzys = driverNameArray.filter(driver => driver.startsWith(string));
+    return fuzzys
+}
+
+function matchName(arrayOfDriverObj, string){
+   let matchesObj = arrayOfDriverObj.filter(driver => driver.name === string)
+   return matchesObj
+}
